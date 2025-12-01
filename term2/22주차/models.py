f667b55e910f791edca3602a0cf0b0da80d1feb3
nk329 <zeroorder31@gmail.com>
2025-12-01 20:02:15 +0900

    FastAPI question 라우터 및 ORM 연동 추가

---
+++ term2/22주차/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, DateTime, Integer, String, Text
+
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = 'question'
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String(200), nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+
+
